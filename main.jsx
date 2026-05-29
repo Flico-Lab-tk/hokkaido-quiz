b7dfd8097c7c7b91f3d29d12677c1fff607d6723
takahiro kamada <takahiro.kamada@flico-lab.com>
2026-05-30 06:55:14 +0900

    feat: よめるべ！北海道！ 初回リリース
    - 北海道全179市町村タイピングゲーム
    - ランダム10問（易しい・普通・難しい）モード
    - 全179市町村モード・エリア別モード
    - 市・町・村は入力不要
    - 正解率に応じたリザルト表示

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import HokkaidoTypingGame from "./HokkaidoTypingGame.jsx";
+
+createRoot(document.getElementById("root")).render(<HokkaidoTypingGame />);
